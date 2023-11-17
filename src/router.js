3eaffb36dd4526d485dd0f2a1f4d83ce86d6e62b
Rafael Nascimento <rafavnc1981@gmail.com>
2023-11-16 23:20:32 -0300

    Removed functions and created selectors file to fix the homepage button

--- src/router.js
+++ src/router.js
@@ -1,3 +1,5 @@
+import * as selectors from "./selectors.js"
+
 export class Router {
 
   routes = {}
@@ -19,8 +21,36 @@ export class Router {
   handle() {
     const { pathname } = window.location
     const route = this.routes[pathname] || this.routes[404]
+
+    if(pathname === "/") {
+      selectors.addBackgroundImage.classList.remove('exploration-background')
+      selectors.addBackgroundImage.classList.add('home-background')
+      selectors.addBackgroundImage.classList.remove('universe-background')
+
+      selectors.exploration.classList.remove('selected')
+      selectors.universe.classList.remove('selected')
+      selectors.home.classList.add('selected')
+    } else if ( pathname === "/universe"){
+      selectors.addBackgroundImage.classList.remove('exploration-background')
+      selectors.addBackgroundImage.classList.remove('home-background')
+      selectors.addBackgroundImage.classList.add('universe-background')
+
+      selectors.exploration.classList.remove('selected')
+      selectors.universe.classList.add('selected')
+      selectors.home.classList.remove('selected')
+    } else if ( pathname === "/exploration") {
+      selectors.addBackgroundImage.classList.add('exploration-background')
+      selectors.addBackgroundImage.classList.remove('home-background')
+      selectors.addBackgroundImage.classList.remove('universe-background')
+
+      selectors.exploration.classList.add('selected')
+      selectors.universe.classList.remove('selected')
+      selectors.home.classList.remove('selected')
+    }
+
     fetch(route)
     .then(data => data.text())
     .then(html => document.querySelector('#pages').innerHTML = html)
   }
 }
+
---
+++ src/selectors.js
@@ -0,0 +1,4 @@
+export const home = document.getElementById('homepage')
+export const universe = document.getElementById('universePage')
+export const exploration = document.getElementById('explorationPage')
+export const addBackgroundImage = document.querySelector('body')
